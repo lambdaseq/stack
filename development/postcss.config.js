c140cedf35ec89cb2a9a05046f6feaaebd4e5ffc
DrBuchkov <pafitisnick@gmail.com>
2024-08-18 19:25:55 +0300

    Adding configuration in development project for compiling landing page tailwind css from clojure source files.

--- development/postcss.config.js
+++ development/postcss.config.js
@@ -1,6 +1,15 @@
-module.exports = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    }
+module.exports = (_env, _options) => {
+    // Figure out which type of project we are building
+    const projectType = process.env.PROJECT_TYPE || 'app';
+    // Return the appropriate configuration based on the project type
+    const configFile = {
+        app: './tailwind.app.config.js',
+        page: './tailwind.page.config.js'
+    }[projectType];
+    return {
+        plugins: {
+            tailwindcss: {config: configFile},
+            autoprefixer: {},
+        }
+    };
 }
---
+++ development/tailwind.page.config.js
@@ -0,0 +1,28 @@
+const presetQuick = require("franken-ui/shadcn-ui/preset-quick");
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+    // Configure to scan clojure files for use in server side applications (with hiccup for example)
+    content: {
+        files: [
+            // TODO: Add your own files to scan here
+            './src/**/*.{clj,cljs,cljc}'
+        ],
+        extract: {
+            clj: (content) => scanClojure(content),
+            cljs: (content) => scanClojure(content),
+            cljc: (content) => scanClojure(content)
+        }
+    },
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+    ],
+    presets: [presetQuick()]
+}
