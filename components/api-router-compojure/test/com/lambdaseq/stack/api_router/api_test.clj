fbe8d554d3d04e82178cdda4b9cb8d4e29e79d07
DrBuchkov <pafitisnick@gmail.com>
2024-08-21 02:49:30 +0300

    Fixes in deps

--- components/api-router-compojure/test/com/lambdaseq/stack/api_router/api_test.clj
+++ components/api-router-compojure/test/com/lambdaseq/stack/api_router/api_test.clj
@@ -1,6 +1,5 @@
 (ns com.lambdaseq.stack.api-router.api-test
-  (:require [clojure.test :as test :refer :all]
-            [com.lambdaseq.stack.api-router.api :as api-router-compojure]))
+  (:require [clojure.test :refer :all]))
 
 (deftest dummy-test
   (is (= 1 1)))
