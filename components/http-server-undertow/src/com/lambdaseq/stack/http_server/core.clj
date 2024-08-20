e99e45d2a9134b9550470e587f852250c1c49d0f
DrBuchkov <pafitisnick@gmail.com>
2024-08-21 01:13:51 +0300

    Creating and implementing poly component alternative for http server (undertow)

---
+++ components/http-server-undertow/src/com/lambdaseq/stack/http_server/core.clj
@@ -0,0 +1,23 @@
+(ns com.lambdaseq.stack.http-server.core
+  (:require [com.lambdaseq.stack.logging.api :as log]
+            [strojure.undertow.server :as server]))
+
+(defrecord UndertowHttpServer [config handler-provider server]
+  component/Lifecycle
+  (start [this]
+    (log/info! :starting-component {:component this})
+    (-> this
+        (assoc :server (-> config
+                           (assoc :handler (handler/get-handler handler-provider))
+                           (server/start)))))
+  (stop [this]
+    (log/info! :stopping-component {:component this})
+    (when server
+      (server/stop server))
+    (-> this
+        (assoc :config nil)
+        (assoc :handler-provider nil)
+        (assoc :server nil))))
+
+(defn make-server []
+  (map->UndertowHttpServer {}))
---
+++ components/http-server-undertow/test/com/lambdaseq/stack/http_server/api_test.clj
@@ -0,0 +1,6 @@
+(ns com.lambdaseq.stack.http-server.api-test
+  (:require [clojure.test :as test :refer :all]
+            [com.lambdaseq.stack.http-server.api :as http-server]))
+
+(deftest dummy-test
+  (is (= 1 1)))
